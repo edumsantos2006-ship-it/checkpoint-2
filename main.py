00e7a1ff4d333dc1fc4e6d04b7914ca965c4e34a
eduardo marques dos santos <edumsantos2006@gmail.com>
2026-04-22 21:33:45 -0300

    continuação checkpoint 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from tarefas import adicionar_tarefas, listar_tarefas
+from tarefas import adicionar_tarefas, listar_tarefas,concluir_tarefa
 
 
 while True:
     print("-- [1]Adicionar --")
     print("-- [2]listar --")
+    print("-- [3]conluir tarefas --")
     print("-- [0]sair --")
 
     opcao = int(input("escolha uma das opções:"))
@@ -15,7 +16,17 @@ while True:
             adicionar_tarefas(descricao)
         case 2:
             listar_tarefas()
+            
+        case 3:
+            try:
+                indice = int(input("digite o número da tarefa que deseja concluir:"))
+                concluir_tarefa(indice)
+
+            except ValueError:
+                print("falha, tente um número válido !!")
+            
         case 0:
+            print("saindo...")
             break
         case _:
             print("opção inválida")
--- tarefas.py
+++ tarefas.py
@@ -4,20 +4,28 @@ lista_tarefas = []
 def adicionar_tarefas(descricao):
 
     tarefa = {
-        "descrição" :descricao,
-        "concluída" : False
+        "descricao" :descricao,
+        "concluida" : False
     }
 
     lista_tarefas.append(tarefa)
 
 
 def listar_tarefas():
-    for descricao in lista_tarefas:
-        print(descricao)
-        
+    for indice, tarefa in enumerate(lista_tarefas):
+        status = '[x]' if tarefa['concluido'] else '[]'
+        print(f"{indice}. {tarefa['descricao']} [{status}]")
 
-def concluir_tarefa(indice)
 
+def concluir_tarefa(indice):
+    try:
+        lista_tarefas[indice]["concluida"] = True
+
+    except IndexError:
+        print("erro")
+
+     
 
 print(adicionar_tarefas)
 print(listar_tarefas)
+print(concluir_tarefa)
